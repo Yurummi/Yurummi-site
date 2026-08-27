7b714ec0df7f74c65b998942d21a02cb9ea4583c
Yurummi <dizboi321@gmail.com>
2026-08-27 02:02:20 +0200

    feat: add new sketch and art images, mark sonic-adv-2 as completed

--- js/sonic-checklist.js
+++ js/sonic-checklist.js
@@ -42,7 +42,8 @@ const SONIC_GAME_STATES = {
     "sonic-4-ep1": "completed",
     "sonic-4-ep2": "completed",
     "sonic-adv-1": "completed",
-    "sonic-adv-2": "playing"
+    "sonic-adv-2": "completed",
+    "sonic-adv-3": "playing"
 };
 
 const totalGames = SONIC_GAMES.filter(g => !g.isCategory).length;
