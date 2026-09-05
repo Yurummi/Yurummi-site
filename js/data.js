0921cf79edafedb622137e516cca774f42fed7e4
Yurummi <dizboi321@gmail.com>
2026-09-05 19:37:09 +0200

    chore: cancel stream on sep 5

--- js/data.js
+++ js/data.js
@@ -165,7 +165,7 @@ const STREAMS_LIST = [
     { date: '2026-09-01', time: '15:00', text: 'Соник', isSonic: true, isCanceled: false },
     { date: '2026-09-02', time: '15:00', text: 'Соник', isSonic: true, isCanceled: true },
     { date: '2026-09-04', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
-    { date: '2026-09-05', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
+    { date: '2026-09-05', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: true },
 
     // Периодические события
     { startDate: '2026-07-06', endDate: '2026-07-17', text: 'ОТПУСК 🌴', type: 'vacation' }
