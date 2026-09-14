97b62731ab0620ecdc47095bbd2696e79f3d986e
Yurummi <dizboi321@gmail.com>
2026-09-14 19:21:52 +0200

    chore: adjust schedule and mark sonic marathon as paused

--- js/data.js
+++ js/data.js
@@ -172,10 +172,10 @@ const STREAMS_LIST = [
     { date: '2026-09-11', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
     { date: '2026-09-12', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
     { date: '2026-09-14', time: '15:00', text: 'Соник', isSonic: true, isCanceled: false },
-    { date: '2026-09-15', time: '15:00', text: 'Соник', isSonic: true, isCanceled: false },
-    { date: '2026-09-16', time: '15:00', text: 'Соник', isSonic: true, isCanceled: false },
-    { date: '2026-09-18', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
-    { date: '2026-09-19', time: '±20:00', text: 'Соник', isSonic: true, isCanceled: false },
+    { date: '2026-09-15', time: '15:00', text: 'Стрим', isSpecial: false, isCanceled: false },
+    { date: '2026-09-16', time: '15:00', text: 'Стрим', isSpecial: false, isCanceled: true },
+    { date: '2026-09-18', time: '±20:00', text: 'Стрим', isSpecial: false, isCanceled: false },
+    { date: '2026-09-19', time: '±20:00', text: 'Стрим', isSpecial: false, isCanceled: false },
 
     // Периодические события
     { startDate: '2026-07-06', endDate: '2026-07-17', text: 'ОТПУСК 🌴', type: 'vacation' }
