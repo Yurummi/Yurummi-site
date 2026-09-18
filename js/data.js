cdd76decaebbd9b19acfbead4f8c5f00ac665e26
Yurummi <dizboi321@gmail.com>
2026-09-18 22:50:33 +0200

    fix(schedule): remove streams on Sept 18 and 19

--- js/data.js
+++ js/data.js
@@ -167,8 +167,7 @@ const STREAMS_LIST = [
     { date: '2026-09-14', time: '15:00', text: 'Соник', isSonic: true, isCanceled: false },
     { date: '2026-09-15', time: '15:15', text: 'Рисование', isArt: true, isCanceled: false },
     { date: '2026-09-16', time: '20:00', text: 'Рисование', isArt: true, isCanceled: false },
-    { date: '2026-09-18', time: '±20:00', text: 'Стрим', isSpecial: false, isCanceled: false },
-    { date: '2026-09-19', time: '±20:00', text: 'Стрим', isSpecial: false, isCanceled: false },
+
 
     // Периодические события
     { startDate: '2026-07-06', endDate: '2026-07-17', text: 'ОТПУСК 🌴', type: 'vacation' }
